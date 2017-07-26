aceb0b2ac2097fda4de65a432a35c05e8b5cc045
komlenn <30442211+komlenn@users.noreply.github.com>
2017-07-26 03:17:13 +0200

    imbecili

--- app/Views/team.php
+++ app/Views/team.php
@@ -1,4 +1,5 @@
 <!--- team --->
+<?php var_dump($team_members); ?>
 <div id="team" class="team">
   <div class="container">
     <div class="team-head text-center">
@@ -19,32 +20,7 @@
           </ul>
         </div>
       </div>
-      <div class="col-md-4">
-        <div class="team-member">
-          <img src="<?php echo BASE_URL; ?>files/images/t2.png" title="name" />
-          <h5><span>Steve </span>Tracy</h5>
-          <label>CEO</label>
-          <ul class="unstyled-list">
-            <li><a class="twitter" href="#"><span> </span></a></li>
-            <li><a class="facebook" href="#"><span> </span></a></li>
-            <li><a class="linked" href="#"><span> </span></a></li>
-            <div class="clearfix"> </div>
-          </ul>
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="team-member">
-          <img src="<?php echo BASE_URL; ?>files/images/t3.png" title="name" />
-          <h5><span>Hellen </span>Johns</h5>
-          <label>Clients	</label>
-          <ul class="unstyled-list">
-            <li><a class="twitter" href="#"><span> </span></a></li>
-            <li><a class="facebook" href="#"><span> </span></a></li>
-            <li><a class="linked" href="#"><span> </span></a></li>
-            <div class="clearfix"> </div>
-          </ul>
-        </div>
-      </div>
+
       <div class="clearfix"> </div>
     </div>
     <!--- team-members --->
