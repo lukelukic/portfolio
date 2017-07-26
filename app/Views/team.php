cf8340b7b2a46c25f67c8b762961933c952ebb93
komlenn <30442211+komlenn@users.noreply.github.com>
2017-07-26 17:56:06 +0200

    Team php ,tri osobe izmenjene.

--- app/Views/team.php
+++ app/Views/team.php
@@ -1,5 +1,4 @@
 <!--- team --->
-<?php var_dump($team_members); ?>
 <div id="team" class="team">
   <div class="container">
     <div class="team-head text-center">
@@ -7,20 +6,29 @@
     </div>
     <!--- team-members --->
     <div class="team-members text-center">
-      <div class="col-md-4">
-        <div class="team-member">
-          <img src="<?php echo BASE_URL; ?>files/images/t1.png" title="name" />
-          <h5><span>Tomothy </span>Bowman</h5>
-          <label>Marketing</label>
-          <ul class="unstyled-list">
-            <li><a class="twitter" href="#"><span> </span></a></li>
-            <li><a class="facebook" href="#"><span> </span></a></li>
-            <li><a class="linked" href="#"><span> </span></a></li>
-            <div class="clearfix"> </div>
-          </ul>
-        </div>
-      </div>
-
+      <?php if(isset($team_members)): ?>
+        <?php foreach ($team_members as $key => $value):?>
+          <div class="col-md-4">
+            <div class="team-member">
+              <img src="<?php echo BASE_URL; ?>files/images/<?php echo $value->picture; ?>" height="200" title="<?php echo $value->alt; ?>" />
+              <h5><span><?php  echo $value->firstName; ?> </span><?php  echo $value->lastName; ?></h5>
+              <label><?php  echo $value->position; ?></label>
+              <ul class="unstyled-list">
+                <?php if($value->twitter != ""): ?>
+                  <li><a class="twitter" target="_blank" href="https://twitter.com/<?php echo $value->twitter; ?>"><span> </span></a></li>
+                <?php endif; ?>
+                <?php if($value->facebook): ?>
+                  <li><a class="facebook" target="_blank" href="https://facebook.com/<?php echo $value->facebook; ?>"><span> </span></a></li>
+                <?php endif; ?>
+                <?php if($value->linkedIn): ?>
+                  <li><a class="linked" target="_blank" href="https://linkedin.com/<?php echo $value->linkedIn; ?>"><span> </span></a></li>
+                <?php endif; ?>
+                <div class="clearfix"> </div>
+              </ul>
+            </div>
+          </div>
+        <?php endforeach; ?>
+      <?php endif; ?>
       <div class="clearfix"> </div>
     </div>
     <!--- team-members --->
