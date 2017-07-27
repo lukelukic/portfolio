f4575a12752cc5d4851f0d030bb81f9d74c839ca
komlenn <30442211+komlenn@users.noreply.github.com>
2017-07-27 17:30:31 +0200

    Druga forma plus php.

--- app/Views/Admin/users.php
+++ app/Views/Admin/users.php
@@ -27,20 +27,20 @@
              </tr>
              <?php if(isset($team_members)): ?>
                <?php $rb=1; ?>
-               <?php foreach($team_members as $member): ?>
+               <?php foreach($team_members as $tm): ?>
                  <tr>
                    <td><?php echo $rb++; ?></td>
-                   <td><?php echo $member->firstName; ?></td>
-                   <td><?php echo $member->lastName; ?></td>
-                   <td><?php echo $member->position; ?></td>
-                   <td><?php echo $member->linkedIn ? $member->linkedIn : "/"; ?></td>
-                   <td><?php echo $member->facebook ? $member->facebook : "/"; ?></td>
-                   <td><?php echo $member->twitter ? $member->twitter : "/"; ?></td>
-                   <td><?php echo $member->instagram ? $member->instagram : "/"; ?></td>
-                   <td><?php echo $member->picture; ?></td>
-                   <td><?php echo $member->alt; ?></td>
-                   <td><a href="<?php echo BASE_URL; ?>admin/memberEdit/<?php echo $member->id; ?>" class='btn btn-warning btn-sm'>Edit</a></td>
-                   <td><a href="<?php echo BASE_URL; ?>admin/memberDelete/<?php echo $member->id; ?>" class='btn btn-danger btn-sm'>Delete</a></td>
+                   <td><?php echo $tm->firstName; ?></td>
+                   <td><?php echo $tm->lastName; ?></td>
+                   <td><?php echo $tm->position; ?></td>
+                   <td><?php echo $tm->linkedIn ? $tm->linkedIn : "/"; ?></td>
+                   <td><?php echo $tm->facebook ? $tm->facebook : "/"; ?></td>
+                   <td><?php echo $tm->twitter ? $tm->twitter : "/"; ?></td>
+                   <td><?php echo $tm->instagram ? $tm->instagram : "/"; ?></td>
+                   <td><?php echo $tm->picture; ?></td>
+                   <td><?php echo $tm->alt; ?></td>
+                   <td><a href="<?php echo BASE_URL; ?>admin/editMember?id=<?php echo $tm->id; ?>" class='btn btn-warning btn-sm'>Edit</a></td>
+                   <td><a href="<?php echo BASE_URL; ?>admin/deleteMember?id=<?php echo $tm->id; ?>" class='btn btn-danger btn-sm'>Delete</a></td>
                  </tr>
                <?php endforeach; ?>
              <?php endif; ?>
@@ -52,8 +52,62 @@
         <div class="row">
         <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 col-lg-offset-4 col-md-offset-4 ">
 
+          <?php if(isset($_SESSION['delSuccess'])): ?>
+            <div class="alert alert-success alert-dismissible">
+            <?php echo $_SESSION['delSuccess']; ?>
+            </div>
+          <?php endif; ?>
+
+          <?php if(isset($_SESSION['delError'])): ?>
+            <div class="alert alert-danger alert-dismissible">
+              <?php echo $_SESSION['delError']; ?>
+            </div>
+          <?php endif; ?>
+
             <!-- Forma za dodavanje novog korisnika, prikazuje se uvek osim ako je prosledjen edit parametar -->
-              <p class="lead">New team members</p>
+
+            <?php if(isset($member)): ?>
+              <p class="lead">Edit team member</p>
+            <form method="post" action="<?php echo BASE_URL;?>admin/doEdit" enctype="multipart/form-data">
+              <div class="form-group">
+                <input type="text" name="tbFirstName" value="<?php echo $member->firstName; ?>" class="form-control" placeholder="First Name" id="tbFirstName">
+              </div>
+              <div class="form-group">
+                <input type="text" name="tbLastName" value="<?php echo $member->lastName; ?>" class="form-control" placeholder="Last Name"id="tbLastName">
+              </div>
+              <div class="form-group">
+                <input type="text" name="tbPosition" value="<?php echo $member->position; ?>" class="form-control" placeholder="Position" id="tbPosition">
+              </div>
+              <div class="form-group">
+                <input type="text" name="tbLinkedIn" value="<?php echo $member->linkedIn; ?>" class="form-control" placeholder="LinkedIn" id="tbLinkedIn">
+              </div>
+              <div class="form-group">
+                <input type="text" name="tbFacebook" value="<?php echo $member->facebook; ?>" class="form-control" placeholder="Facebook" id="tbFacebook">
+              </div>
+              <div class="form-group">
+                <input type="text" name="tbTwitter" value="<?php echo $member->twitter; ?>" class="form-control" placeholder="Twitter" id="tbTwitter">
+              </div>
+              <div class="form-group">
+                <input type="text" name="tbInstagram" value="<?php echo $member->instagram; ?>" class="form-control" placeholder="Instagram" id="tbInstagram">
+              </div>
+              <p><strong>Picture :</strong></p>
+              <div class="form-group">
+                <input type="file" name="tbPicture" value="" class="form-control" id="tbPicture">
+              </div>
+              <div class="form-group">
+                <input type="text" name="tbAlt" value="<?php echo $member->alt; ?>" class="form-control" placeholder="Alt Attribute" id="tbAlt">
+              </div>
+              <div class="form-group">
+                <input type="submit" class="btn btn-warning" name="btnSubmit" value="Edit member" class="form-control" id="formSubmit">
+              </div>
+            </form>
+
+            <div id="feedback" class="hidden alert alert-danger ">
+
+            </div>
+
+            <?php else: ?>
+              <p class="lead">New team member</p>
             <form method="post" action="<?php echo BASE_URL;?>admin/addMember" enctype="multipart/form-data">
               <div class="form-group">
                 <input type="text" name="tbFirstName" value="" class="form-control" placeholder="First Name" id="tbFirstName">
@@ -91,6 +145,7 @@
             <div id="feedback" class="hidden alert alert-danger ">
 
             </div>
+          <?php endif ?>
 
 
 
