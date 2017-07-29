b00a52b895592bb7657015c36c2c354e76a7dbc2
komlenn <30442211+komlenn@users.noreply.github.com>
2017-07-29 20:19:26 +0200

    okviri

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -8,19 +8,19 @@
         	<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 col-lg-offset-4 col-md-offset-4 footer-nav animated fadeInUp">
             	<form class="" action="index.html" method="post">
             	 <div class="form-group">
-            	   <input type="text" name="mFirstName" value="" placeholder="First Name" class="form-control">
+            	   <input type="text" name="mFirstName" value="" placeholder="First Name" class="form-control okvir">
             	 </div>
                <div class="form-group">
-            	   <input type="text" name="mPhoneNumber" value="" placeholder="Phone Number" class="form-control">
+            	   <input type="text" name="mPhoneNumber" value="" placeholder="Phone Number" class="form-control okvir">
             	 </div>
                <div class="form-group">
-            	   <input type="text" name="mEmail" value="" placeholder="Email" class="form-control">
+            	   <input type="text" name="mEmail" value="" placeholder="Email" class="form-control okvir">
             	 </div>
                <div class="form-group">
-            	   <textarea name="mMessage" rows="8" cols="80" class="form-control" placeholder="Your Message"></textarea>
+            	   <textarea name="mMessage" rows="8" cols="80" class="form-control okvir" placeholder="Your Message"></textarea>
             	 </div>
                <div class="form-group">
-                 <input type="button" name="mSubmit" value="Submit" class="form-control boja ">
+                 <input type="button" name="mSubmit" value="Submit" class=" form-control boja ">
                </div>
             	</form>
             </div>
