9e87b72cd8bb72d0e612da25713b7861bbc5503e
komlenn <30442211+komlenn@users.noreply.github.com>
2017-07-29 17:34:44 +0200

    footer and css

--- app/Views/address.php
+++ app/Views/address.php
@@ -1,6 +1,3 @@
 <!---- Address ---->
-<div id="contact" class="">
-  <div class="map">
-<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4013.896482763197!2d20.193734438308443!3d44.65317375811104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ssr!2srs!4v1501340509980"  frameborder="0" style="border:0" allowfullscreen></iframe>
-</div>
+
 <!---- Address ---->
